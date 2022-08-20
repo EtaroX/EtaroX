3e98e6276f7d76fe1d4bf961f4c0e78c70280715
Etaro <95373414+EtaroX@users.noreply.github.com>
2022-08-21 01:05:59 +0200

    Update index.php

--- docs/index.php
+++ docs/index.php
@@ -1,4 +1,10 @@
-<?php
-header('Content-Type: text/css')
-echo readfile('https://raw.githubusercontent.com/EtaroX/EtaroX/main/Resoures/Github_as_drive_go_brrrr/SecretTest.theme.css')
-?>
+<script>
+const myHeaders = new Headers();
+myHeaders.append('Content-Type', 'text/css');
+
+fetch('https://raw.githubusercontent.com/EtaroX/EtaroX/main/Resoures/Github_as_drive_go_brrrr/SecretTest.theme.css')
+  .then(response => response.text())
+  .then((data) => {
+    document.write(data);
+  })
+</script>
